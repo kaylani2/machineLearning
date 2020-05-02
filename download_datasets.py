45af74a88d47c21d826ca5c0e1ef978a9cdc9ebb
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-05-02 20:53:46 -0300

    Add download_datasets.py

---
+++ download_datasets.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+CICIDS_URL = '205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip'
+NSLKDD_URL = '205.174.165.80/CICDataset/NSL-KDD/Dataset/NSL-KDD.zip'
+
+#urls = [CICIDS_URL, NSLKDD_URL]
+urls = []
+
+for url in urls:
+  os.system ('wget ' + url)
+
+#os.system ('mv MachineLearningCSV.zip datasets/cicids/')
+#os.system ('mv NSL-KDD.zip datasets/nslkdd/')
+os.chdir ('datasets/cicids/')
+os.system ('unzip MachineLearningCSV.zip')
+os.chdir ('../nslkdd/')
+os.system ('unzip NSL-KDD.zip')
+os.chdir ('../../') #root
+sys.exit ()
