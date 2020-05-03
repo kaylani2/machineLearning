9e6589ccda8c1dac1ee5c9ca9e63d5e41c260c2a
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-05-03 20:45:28 -0300

    Finish dataset split

--- src/dataset_analysis/visualize_datasets.py
+++ src/dataset_analysis/visualize_datasets.py
@@ -2,10 +2,11 @@
 # github.com/kaylani2
 # kaylani AT gta DOT ufrj DOT br
 
-## Carregar dataset, descrever, tratar atributos categoricos e plotar graficos
-## CICIDS usado como exemplo por enquanto
+## Load dataset, describe, hadle categorical attributes
+## CICIDS used as an example
 
 import pandas as pd
+import numpy as np
 import sys
 
 ## Hard to not go over 80 columns
@@ -20,9 +21,6 @@ CICIDS_WEDNESDAY = CICIDS_DIRECTORY + CICIDS_WEDNESDAY_FILENAME
 ## Load dataset
 ###############################################################################
 df = pd.read_csv (CICIDS_WEDNESDAY)
-#trainFrame = pd.read_csv (TRAIN_FILE)#, header = None)
-#testFrame = pd.read_csv (TEST_FILE)#, header = None)
-#df = pd.concat ([trainFrame, testFrame], ignore_index = True)
 
 ###############################################################################
 ## Display generic (dataset independent) information
@@ -31,14 +29,17 @@ print ('Dataframe shape (lines, collumns):', df.shape, '\n')
 print ('First 5 entries:\n', df [:5], '\n')
 print ('Dataframe attributes:\n', df.keys (), '\n')
 ## Note the pesky spaces before ALMOST all attributes
-## This is annoying and could be removed, but will try to operate on the
+## This is annoying and could be removed, but we'll try to operate on the
 ## dataset "as is"
 df.info (verbose = False) # Make it true to find individual atribute types
-print (df.describe ()) # Brief statistical description on NUMERIC atributes
+print (df.describe ()) # Brief statistical description on NUMERICAL atributes
+print ('Dataframe contains NaN values:', df.isnull ().values.any ())
+nanColumns = [i for i in df.columns if df [i].isnull ().any ()]
+print ('NaN columns:', nanColumns)
+
 ## Reminder: pearson only considers numerical atributes (ignores catgorical)
 #correlationMatrix =  df.corr (method = 'pearson')
 #print ('Pearson:', correlationMatrix)
-
 ## You may want to plot the correlation matrix, but it gets hard to read
 ## when you have too many attributes. It's probably better to get the values
 ## you want with a set threshold directly from the matrix.
@@ -55,24 +56,55 @@ print (df.describe ()) # Brief statistical description on NUMERIC atributes
 ## Remember the pesky spaces?
 print ('Label types:', df [' Label'].unique ())
 
+
 ###############################################################################
-## Converter dataframe para arrays numpy (usualmente a ultima coluna eh o alvo)
+## Perform some form of basic preprocessing
+###############################################################################
+## For basic feature selection the correlation matrix can help identify
+## highly correlated features (which are bad/cursed). In order to find
+## which features have the highest predictive power, it's necessary
+## to convert the target to a numeric value. After that it's possible to use
+## a simple filter approach or a wrapper method (backward elimination, forward
+## selection...) to select features.
+## You may also choose to convert the dataframe to a numpy array and continue.
+
+## Remove NaN and inf values
+df.replace ('Infinity', np.nan, inplace = True) ## Or other text values
+df.replace (np.inf, np.nan, inplace = True) ## Remove infinity
+df.replace (np.nan, 0, inplace = True)
+## We can also use scikit-learn to use other strategies for substitution
+print ('Dataframe contains NaN values:', df.isnull ().values.any ())
+nanColumns = [i for i in df.columns if df [i].isnull ().any ()]
+print ('NaN columns:', nanColumns)
+
+###############################################################################
+## Encode categorical attributes (this may be done before finding pearson)
+###############################################################################
+print ('Label types before conversion:', df [' Label'].unique ())
+df [' Label'] = df [' Label'].replace ('BENIGN', 0)
+df [' Label'] = df [' Label'].replace ('DoS slowloris', 1)
+df [' Label'] = df [' Label'].replace ('DoS Slowhttptest', 2)
+df [' Label'] = df [' Label'].replace ('DoS Hulk', 3)
+df [' Label'] = df [' Label'].replace ('DoS GoldenEye', 4)
+df [' Label'] = df [' Label'].replace ('Heartbleed', 5)
+print ('Label types after conversion:', df [' Label'].unique ())
+df.info (verbose = False)
+
+###############################################################################
+## Convert dataframe to a numpy array (usually the last column is the target)
 ###############################################################################
 X = df.iloc [:, :-1].values
 y = df.iloc [:, -1].values
-sys.exit ()
 
 ###############################################################################
-## Tratar atributos categóricos
+## Split dataset into train and test sets
 ###############################################################################
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder ()
-X[:, 1] = labelencoder_X.fit_transform (X[:, 1])
-X[:, 2] = labelencoder_X.fit_transform (X[:, 2])
-X[:, 3] = labelencoder_X.fit_transform (X[:, 3])
-labelencoder_y = LabelEncoder ()
-y = labelencoder_y.fit_transform (y)
-print ('Informacao:')
-df.info (verbose = True)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split (X, y, test_size = 1/3,
+                                                     random_state = 0)
+print ('X_train shape:', X_train.shape)
+print ('y_train shape:', y_train.shape)
+print ('X_test shape:', X_test.shape)
+print ('y_test shape:', y_test.shape)
 
 sys.exit ()
