380f298f60ec1dd36b17534d157cffa007dd5539
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-05-02 23:00:25 -0300

    Refactoring

--- src/download_datasets.py
+++ src/download_datasets.py
@@ -3,15 +3,18 @@ import sys
 
 CICIDS_URL = '205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip'
 NSLKDD_URL = '205.174.165.80/CICDataset/NSL-KDD/Dataset/NSL-KDD.zip'
+urls = [CICIDS_URL, NSLKDD_URL]
 
-#urls = [CICIDS_URL, NSLKDD_URL]
-urls = []
+
+
+os.chdir ('../') #root
+os.system ('mkdir datasets datasets/cicids datasets/nslkdd')
 
 for url in urls:
   os.system ('wget ' + url)
 
-#os.system ('mv MachineLearningCSV.zip datasets/cicids/')
-#os.system ('mv NSL-KDD.zip datasets/nslkdd/')
+os.system ('mv MachineLearningCSV.zip datasets/cicids/')
+os.system ('mv NSL-KDD.zip datasets/nslkdd/')
 os.chdir ('datasets/cicids/')
 os.system ('unzip MachineLearningCSV.zip')
 os.chdir ('../nslkdd/')
