3d9065b453eae0076e0b8a9738461b82789a5534
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-05-14 20:26:59 -0300

    Add unsw-nb15

--- src/download_datasets.py
+++ src/download_datasets.py
@@ -1,23 +1,35 @@
+## TODO: Compare hashes
+## TODO: Fix unsw-nb15 formatting
+
 import os
 import sys
 
-CICIDS_URL = '205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip'
-NSLKDD_URL = '205.174.165.80/CICDataset/NSL-KDD/Dataset/NSL-KDD.zip'
-urls = [CICIDS_URL, NSLKDD_URL]
+CICIDS = {
+  'url' : '205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip',
+  'dir' : 'datasets/cicids'
+}
 
+NSLKDD = {
+  'url' : '205.174.165.80/CICDataset/NSL-KDD/Dataset/NSL-KDD.zip',
+  'dir' : 'datasets/nslkdd'
+}
 
+UNSW_NB15 = {
+  'url' : 'https://cloudstor.aarnet.edu.au/plus/s/2DhnLGDdEECo4ys/download?path=%2F&files=UNSW-NB15%20-%20CSV%20Files&downloadStartSecret=4j3twms4roi',
+  'dir' : 'datasets/unsw-nb15'
+}
+
+DATASETS = [CICIDS, NSLKDD, UNSW-NB15]
 
 os.chdir ('../') #root
-os.system ('mkdir datasets datasets/cicids datasets/nslkdd')
+os.system ('mkdir datasets')
+for dataset in DATASETS:
+  os.system ('mkdir ' + dataset ['dir'])
+  os.chdir (dataset ['dir'])
+  os.system ('wget ' + dataset ['url'])
+  os.system ('unzip *')
+  os.chdir ('../') #datasets
 
-for url in urls:
-  os.system ('wget ' + url)
+os.chdir ('../') #root
 
-os.system ('mv MachineLearningCSV.zip datasets/cicids/')
-os.system ('mv NSL-KDD.zip datasets/nslkdd/')
-os.chdir ('datasets/cicids/')
-os.system ('unzip MachineLearningCSV.zip')
-os.chdir ('../nslkdd/')
-os.system ('unzip NSL-KDD.zip')
-os.chdir ('../../') #root
 sys.exit ()
