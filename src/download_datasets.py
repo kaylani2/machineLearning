460a8c80870dc4907d13603a21d3935179190c78
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-05-25 18:12:50 -0300

    Implement hash comparison

--- src/download_datasets.py
+++ src/download_datasets.py
@@ -1,25 +1,31 @@
-## @TODO: Compare hashes
-
 import os
 import sys
 import wget
+import hashlib
 
 CICIDS = {
-  'url' : '205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip',
-  'dir' : 'datasets/cicids'
+  'url'  : '205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip',
+  'dir'  : 'datasets/cicids',
+  'name' : 'MachineLearningCSV.zip',
+  'hash' : 'c3f26274b36c837ccf28ffd2dbf4582941c30b3ee70a635c6e5b2f87c4727928'
 }
 
 NSLKDD = {
-  'url' : '205.174.165.80/CICDataset/NSL-KDD/Dataset/NSL-KDD.zip',
-  'dir' : 'datasets/nslkdd'
+  'url'  : '205.174.165.80/CICDataset/NSL-KDD/Dataset/NSL-KDD.zip',
+  'dir'  : 'datasets/nslkdd',
+  'name' : 'NSL-KDD.zip',
+  'hash' : 'b28a4ac1ab5b3f659d251d628f297630f9f0ac61403c4abf18776439553addb0'
 }
 
 UNSW_NB15 = {
-  'url' : 'https://cloudstor.aarnet.edu.au/plus/s/2DhnLGDdEECo4ys/download?path=%2F&files=UNSW-NB15%20-%20CSV%20Files&downloadStartSecret=4j3twms4roi',
-  'dir' : 'datasets/unsw-nb15'
+  'url'  : 'https://cloudstor.aarnet.edu.au/plus/s/2DhnLGDdEECo4ys/download?path=%2F&files=UNSW-NB15%20-%20CSV%20Files&downloadStartSecret=4j3twms4roi',
+  'dir'  : 'datasets/unsw-nb15',
+  'name' : 'UNSW-NB15%20-%20CSV%20Files.zip',
+  'hash' : '4263563348ee50d3bfe327aa4dd360367f7c4b2f37e487838a579123cc068a83'
 }
 
 DATASETS = [CICIDS, NSLKDD, UNSW_NB15]
+
 
 os.chdir ('../') #root
 os.system ('mkdir datasets')
@@ -28,8 +34,18 @@ for dataset in DATASETS:
   os.chdir (dataset ['dir'])
   wget.download (dataset ['url'], './')
   os.system ('unzip *')
-  os.chdir ('../') #datasets
 
-os.chdir ('../') #root
+  ## Compare hashes
+  BLOCK_SIZE = 65536
+  fileHash = hashlib.sha256 ()
+  with open (dataset ['name'], 'rb') as f:
+    fileBlock = f.read (BLOCK_SIZE)
+    while (len (fileBlock) > 0):
+      fileHash.update (fileBlock)
+      fileBlock = f.read (BLOCK_SIZE)
+  if (fileHash.hexdigest () != dataset ['hash']):
+    print ('File downloaded does no match!')
+
+  os.chdir ('../../') #root
 
 sys.exit ()
