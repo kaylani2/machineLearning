78b6a7fa101d96f381f11460273fccdc2c854756
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-06-28 13:41:40 -0300

    Alter plot

--- src/plot_functions/plot_bar.py
+++ src/plot_functions/plot_bar.py
@@ -2,9 +2,16 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-labels = ['SVM', 'MLP', '1D-CNN', 'RNN', 'LSTM']
-acc = [89, 92, 94, 91, 99]
-acc_std = [5, 9, 2, 11, 2]
+#DDoS              1926624
+#DoS               1650260
+#Reconnaissance      91082
+#Normal                477
+#Theft                  79
+
+labels = ['DDoS', 'DoS', 'Reconnaissance', 'Normal', 'Theft']
+acc = [1926624, 1650260, 91082, 477, 79]
+acc_std = [0,0,0,0,0]
+#acc_std = [5, 9, 2, 11, 2]
 recall = [85, 94, 88, 89, 99]
 
 x = np.arange (len (labels))  # the label locations
@@ -14,15 +21,15 @@ fig, ax = plt.subplots ()
 
 #plt.errorbar (x - width/2, acc, mfc = 'green', mec = 'red')
 
-rects1 = ax.bar (x - width/2, acc, width, alpha = 0.8, color = 'black',
-                 label = 'acc', yerr = acc_std,
+rects1 = ax.bar (x , acc, width, alpha = 0.8, color = 'black',
+                 label = '', yerr = acc_std,
                  error_kw = dict (elinewidth = 5, ecolor = 'red'))
 
-rects2 = ax.bar (x + width/2, recall, width, label = 'recall')
+#rects2 = ax.bar (x + width/2, recall, width, label = 'recall')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel ('Percentual')
-ax.set_title ('Desempenho dos modelos profundos')
+ax.set_ylabel ('Amostras')
+ax.set_title ('Quantidade de amostras')
 ax.set_xticks (x)
 ax.set_xticklabels (labels)
 ax.legend ()
@@ -40,7 +47,7 @@ def autolabel (rects):
 
 
 autolabel (rects1)
-autolabel (rects2)
+#autolabel (rects2)
 
 fig.tight_layout ()
 plt.savefig ('bar.png')
