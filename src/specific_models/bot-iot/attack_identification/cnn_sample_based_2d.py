70468a9bbe87a842be93841829146d52faafd92f
Kaylani <kaylani@gta.ufrj.br>
2020-07-11 14:02:50 -0300

    Autoencoder bot-iot running hyper tuning

--- src/specific_models/bot-iot/attack_identification/cnn_sample_based_2d.py
+++ src/specific_models/bot-iot/attack_identification/cnn_sample_based_2d.py
@@ -2,7 +2,7 @@
 # github.com/kaylani2
 # kaylani AT gta DOT ufrj DOT br
 
-### K: Model: Autoencoder
+### K: Model: 2D CNN
 
 import pandas as pd
 import numpy as np
@@ -19,7 +19,6 @@ from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
 from keras.optimizers import Adam
 from keras.wrappers.scikit_learn import KerasClassifier
-from keras.wrappers.scikit_learn import KerasRegressor
 from keras import metrics
 from keras.constraints import maxnorm
 from keras.models import Sequential
@@ -227,51 +226,29 @@ df.drop (axis = 'columns', columns = 'subcategory', inplace = True)
 ###############################################################################
 ## Split dataset into train, validation and test sets
 ###############################################################################
-### Isolate attack and normal samples
-mask = df [TARGET] == 0
-# 0 == normal
-df_normal = df [mask]
-# 1 == attack
-df_attack = df [~mask]
-
-print ('Attack set:')
-print (df_attack [TARGET].value_counts ())
-print ('Normal set:')
-print (df_normal [TARGET].value_counts ())
-
-### Sample and drop random attacks
-df_random_attacks = df_attack.sample (n = df_normal.shape [0], random_state = STATE)
-df_attack = df_attack.drop (df_random_attacks.index)
-
-### Assemble test set
-df_test = pd.DataFrame ()
-df_test = pd.concat ([df_test, df_normal])
-df_test = pd.concat ([df_test, df_random_attacks])
-print ('Test set:')
-print (df_test [TARGET].value_counts ())
-X_test_df = df_test.iloc [:, :-1]
-y_test_df = df_test.iloc [:, -1]
-### K: y_test is required to plot the roc curve in the end
-
-
-
-df_train = df_attack
+from sklearn.model_selection import train_test_split
+TEST_SIZE = 2/10
 VALIDATION_SIZE = 1/4
+print ('\nSplitting dataset (test/train):', TEST_SIZE)
+X_train_df, X_test_df, y_train_df, y_test_df = train_test_split (
+                                               df.iloc [:, :-1],
+                                               df.iloc [:, -1],
+                                               test_size = TEST_SIZE,
+                                               random_state = STATE,)
+                                               #shuffle = False)
 print ('\nSplitting dataset (validation/train):', VALIDATION_SIZE)
 X_train_df, X_val_df, y_train_df, y_val_df = train_test_split (
-                                             df_train.iloc [:, :-1],
-                                             df_train.iloc [:, -1],
+                                             X_train_df,
+                                             y_train_df,
                                              test_size = VALIDATION_SIZE,
                                              random_state = STATE,)
                                              #shuffle = False)
-
-
-#X_train_df.sort_index (inplace = True)
-#y_train_df.sort_index (inplace = True)
-#X_val_df.sort_index (inplace = True)
-#y_val_df.sort_index (inplace = True)
-#X_test_df.sort_index (inplace = True)
-#y_test_df.sort_index (inplace = True)
+X_train_df.sort_index (inplace = True)
+y_train_df.sort_index (inplace = True)
+X_val_df.sort_index (inplace = True)
+y_val_df.sort_index (inplace = True)
+X_test_df.sort_index (inplace = True)
+y_test_df.sort_index (inplace = True)
 #X_train_df.sort_values  (by = 'pkSeqID', inplace = True)
 print ('X_train_df shape:', X_train_df.shape)
 print ('y_train_df shape:', y_train_df.shape)
@@ -279,7 +256,6 @@ print ('X_val_df shape:', X_val_df.shape)
 print ('y_val_df shape:', y_val_df.shape)
 print ('X_test_df shape:', X_test_df.shape)
 print ('y_test_df shape:', y_test_df.shape)
-
 
 
 ###############################################################################
@@ -331,85 +307,63 @@ print (str (time.time () - startTime), 'to normalize data.')
 
 
 ###############################################################################
-## Create learning model (Autoencoder) and tune hyperparameters
+## Create learning model (2D CNN) and tune hyperparameters
 ###############################################################################
+
+###############################################################################
+## Data reshaping
+SAMPLE_2D_SIZE = X_train.shape [1] # 7x7
+
+X_train.resize ( (X_train.shape[0], SAMPLE_2D_SIZE, SAMPLE_2D_SIZE))
+X_train = X_train.reshape ( (X_train.shape[0], 7, 7, 1))
+X_val.resize ( (X_val.shape[0], SAMPLE_2D_SIZE, SAMPLE_2D_SIZE))
+X_val = X_val.reshape ( (X_val.shape[0], 7, 7, 1))
+X_test.resize ( (X_test.shape[0], SAMPLE_2D_SIZE, SAMPLE_2D_SIZE))
+
+print (train_features.shape)
+
 
 ###############################################################################
 ## Hyperparameter tuning
 test_fold = np.repeat ([-1, 0], [X_train.shape [0], X_val.shape [0]])
 myPreSplit = PredefinedSplit (test_fold)
-def create_model (learn_rate = 0.01, dropout_rate = 0.0, weight_constraint = 0):
-  model = Sequential ()
-  model.add (Dense (X_train.shape [1], activation = 'relu',
-                    input_shape = (X_train.shape [1], )))
-  model.add (Dense (32, activation = 'relu'))
-  model.add (Dense (8,  activation = 'relu'))
-  model.add (Dense (32, activation = 'relu'))
-  model.add (Dense (X_train.shape [1], activation = None))
-  model.compile (loss = 'mean_squared_error',
-                 optimizer = 'adam',
-                 metrics = ['mse'])
-  return model
-
-model = KerasRegressor (build_fn = create_model, verbose = 2)
-batch_size = [10, 30, 50]
-epochs = [3, 5]#, 5, 10]
-learn_rate = [0.001, 0.01, 0.1]#, 0.2, 0.3]
-dropout_rate = [0.0, 0.2, 0.5]#, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-weight_constraint = [0]#1, 2, 3, 4, 5]
-param_grid = dict (batch_size = batch_size, epochs = epochs,
-                   dropout_rate = dropout_rate, learn_rate = learn_rate,
-                   weight_constraint = weight_constraint)
-grid = GridSearchCV (estimator = model, param_grid = param_grid,
-                     scoring = 'neg_mean_squared_error', cv = myPreSplit,
-                     verbose = 2, n_jobs = -1)
-
-grid_result = grid.fit (np.vstack ( (X_train, X_val)),#, axis = 1),
-                        np.vstack ( (X_train, X_val)))#, axis = 1))
-print (grid_result.best_params_)
-
-print ("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip (means, stds, params):
-  print ("%f (%f) with: %r" % (mean, stdev, param))
-
-sys.exit ()
-
 
 ###############################################################################
 ## Finished model
-NUMBER_OF_EPOCHS = 2
-BATCH_SIZE = 32
-INPUT_SHAPE = (X_train.shape [1], )
 
-print ('\nCreating learning model.')
-### *64*8*64 autoencoder
-bestModel = Sequential ()
-bestModel.add (Dense (X_train.shape [1], activation = 'relu',
-                      input_shape = (X_train.shape [1], )))
-bestModel.add (Dense (32, activation = 'relu'))
-bestModel.add (Dense (8,  activation = 'relu'))
-bestModel.add (Dense (32, activation = 'relu'))
-bestModel.add (Dense (X_train.shape [1], activation = None))
 
+model = models.Sequential ()
+model.add (layers.Conv2D (64, (3, 3), activation = 'relu',
+                          input_shape = (7, 7, 1),
+                          kernel_initializer = initializer))
+model.add (layers.MaxPooling2D ( (3, 3)))
+model.add (layers.Conv2D (64, (2, 2), activation = 'relu'))
+model.add (layers.MaxPooling2D ( (2, 2)))
+model.add (layers.Flatten ())
+model.add (layers.Dense (64, activation = 'relu',
+                         kernel_initializer = initializer))
+model.add (layers.Dense (1, activation = 'sigmoid',
+                         kernel_initializer = initializer))
 
 ###############################################################################
 ## Compile the network
 ###############################################################################
-print ('\nCompiling the network.')
-bestModel.compile (loss = 'mean_squared_error',
-                   optimizer = 'adam',
-                   metrics = ['mse'])#,metrics.Precision ()])
+LEARNING_RATE = 0.01
+model.compile (optimizer = keras.optimizers.Adam (lr = LEARNING_RATE),
+               loss = keras.losses.BinaryCrossentropy (),
+               metrics = ['binary_accuracy', metrics.Precision ()])
 
-print ('Model summary:')
-bestModel.summary ()
+model.summary ()
+
+
+
 
 
 ###############################################################################
 ## Fit the network
 ###############################################################################
+NUMBER_OF_EPOCHS = 2
+BATCH_SIZE = 32
 print ('\nFitting the network.')
 startTime = time.time ()
 history = bestModel.fit (X_train, X_train,
