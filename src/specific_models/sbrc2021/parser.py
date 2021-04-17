20c0109f8e49363e3fbe0e7c249e1215f646bcfb
Kaylani <kaylani@gta.ufrj.br>
2021-04-17 12:40:39 -0300

    Finish 5 clients

--- src/specific_models/sbrc2021/parser.py
+++ src/specific_models/sbrc2021/parser.py
@@ -5,14 +5,37 @@ import statistics
 if __name__ == "__main__":
   try:
     #filename = (sys.argv [1])
-    filename = './logs/10e_64b.log'
+    filename = '10e_64b.log'
   except:
     print ('Usage: $python parser.py FILENAME.log')
     exit ()
 
   results = []
+  accuracies = []
+  standard_deviations = []
   with open (filename) as f:
     for line in f:
-      if (re.search ('32/32', line)):
-        results.append (line)
-        print (line)
+      if ( (re.search ('32/32', line)) and not (re.search ('ETA', line)) ):
+        current_line = line.replace ('\n', '')
+        current_line = current_line [current_line.find ('accuracy: '):]
+        current_line = current_line [len ('accuracy: '):]
+        #print (current_line)
+        results.append (float (current_line))# * 100) # point notation to percentual notation
+      if (re.search ('Number of clients:', line)):
+        number_of_clients = int (line.split (' ') [-1])
+
+  ### K: At the end the framework performs a test on each client.
+  for _ in range (number_of_clients):
+    results.pop ()
+  #print ('Results:', len (results))
+
+  for index in range (0, len (results), 2):
+    local_accuracies = [float (results [index]), float (results [index + 1])]
+    #print (local_accuracies)
+    accuracies.append ((round (statistics.mean (local_accuracies) * 100, 3)))#[:-1]
+    standard_deviations.append ((round (statistics.stdev (local_accuracies) * 100, 3)))#[:-1]
+    #print (index)
+    #print (results [index])
+
+  #mean_accuracy = str (round (statistics.mean (accuracy) * 100, 3))[:-1]
+  #stdev_accuracy = str (round (statistics.stdev (accuracy) * 100, 3))[:-1]
--- src/specific_models/sbrc2021/plot_results.py
+++ src/specific_models/sbrc2021/plot_results.py
@@ -1,25 +1,66 @@
-def found_fit(x):
-  return x**3
-
-B_10_E_1 = [[1, 2, 3, 4, 5], [10, 30, 50, 90, 170]]
-B_10_E_2 = [[1, 2, 3, 4, 5], [8, 9, 20, 30, 32]]
-
+import sys
+import re
+import statistics
 import numpy as np
 from matplotlib import pyplot as plt
 
-x_func = np.linspace(0, 10, 50)
-y_func = found_fit(x_func)
-plt.plot(x_func, y_func, label='$f(x) = x^3$')
+#def found_fit (x):
+#  return x**3
+#
+#if __name__ == "__main__":
+#  try:
+#    #filename = (sys.argv [1])
+#    filename = '10e_64b.log'
+#  except:
+#    print ('Usage: $python parser.py FILENAME.log')
+#    exit ()
 
-plt.plot (B_10_E_1 [0], B_10_E_1 [1], 'rx', label='B=10, E=1')
-plt.plot (B_10_E_2 [0], B_10_E_2 [1], 'b-.', label='B=10, E=2')
+filename = '10e_64b.log'
+my_files = [
+  {'filename': '10e_64b_10c.log', 'label': 'E=10, B=64', 'line_style': 'r-'},
+  {'filename': '20e_64b_10c.log', 'label': 'E=20, B=64', 'line_style': 'b-'},
+]
+for my_file in my_files:
+  print (my_file ['filename'])
+  results = []
+  accuracies = []
+  standard_deviations = []
+  with open (my_file ['filename']) as f:
+    for line in f:
+      if ( (re.search ('32/32', line)) and not (re.search ('ETA', line)) ):
+        current_line = line.replace ('\n', '')
+        current_line = current_line [current_line.find ('accuracy: '):]
+        current_line = current_line [len ('accuracy: '):]
+        #print (current_line)
+        results.append (float (current_line))# * 100) # point notation to percentual notation
+      if (re.search ('Number of clients:', line)):
+        number_of_clients = int (line.split (' ') [-1])
+
+  ### K: At the end the framework performs a test on each client.
+  for _ in range (number_of_clients):
+    results.pop ()
+  #print ('Results:', len (results))
+
+  for index in range (0, len (results), 2):
+    local_accuracies = [float (results [index]), float (results [index + 1])]
+    accuracies.append ( (round (statistics.mean (local_accuracies) * 100, 3)))#[:-1]
+    standard_deviations.append ( (round (statistics.stdev (local_accuracies) * 100, 3)))#[:-1]
+
+
+
+  current_plot = [range (1, len (accuracies) + 1), accuracies, standard_deviations]
+
+  plt.plot (current_plot [0], current_plot [1], my_file ['line_style'], label=my_file ['label'])
+
+plt.axhline (y=67.82, color='r', linestyle='-', label='$67,82\%$')
 plt.legend ()
 plt.xlabel ('Número de rodadas')
 plt.ylabel ('Acurácia')
-plt.xlim (0, 7)
-plt.ylim (0, 200)
+plt.xlim (0, len (accuracies) + 1)
+plt.ylim (0, 100)
 #plt.show ()
-plt.savefig('result.png')
+plt.savefig ('result.png')
+print ('saved')
 
 
 ## subplot:
@@ -29,18 +70,18 @@ plt.savefig('result.png')
 #x_data2 = [0.1, 0.2, 0.3, 0.4]
 #y_data2 = [1, 4, 9, 16]
 #
-#fig = plt.figure()
-#ax1 = fig.add_subplot(1, 2, 1)
-#ax2 = fig.add_subplot(1, 2, 2)
-#ax1.plot(x_data1, y_data1, label='data 1')
-#ax2.plot(x_data2, y_data2, label='data 2')
-#ax1.set_xlabel('Time (s)')
-#ax1.set_ylabel('Scale (Bananas)')
-#ax1.set_title('first data set')
-#ax1.legend()
-#ax2.set_xlabel('Time (s)')
-#ax2.set_ylabel('Scale (Bananas)')
-#ax2.set_title('second data set')
-#ax2.legend()
+#fig = plt.figure ()
+#ax1 = fig.add_subplot (1, 2, 1)
+#ax2 = fig.add_subplot (1, 2, 2)
+#ax1.plot (x_data1, y_data1, label='data 1')
+#ax2.plot (x_data2, y_data2, label='data 2')
+#ax1.set_xlabel ('Time (s)')
+#ax1.set_ylabel ('Scale (Bananas)')
+#ax1.set_title ('first data set')
+#ax1.legend ()
+#ax2.set_xlabel ('Time (s)')
+#ax2.set_ylabel ('Scale (Bananas)')
+#ax2.set_title ('second data set')
+#ax2.legend ()
 #
-#plt.show()
+#plt.show ()
